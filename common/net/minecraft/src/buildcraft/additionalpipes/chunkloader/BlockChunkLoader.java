b18ced821b0cc1787fa535ba3bd328319cca4b59
Kyprus <zachl44@gmail.com>
2012-05-02 15:02:40 -0400

    Added Teleport Tether to the creative inventory.

--- common/net/minecraft/src/buildcraft/additionalpipes/chunkloader/BlockChunkLoader.java
+++ common/net/minecraft/src/buildcraft/additionalpipes/chunkloader/BlockChunkLoader.java
@@ -1,6 +1,9 @@
 package net.minecraft.src.buildcraft.additionalpipes.chunkloader;
 
+import java.util.ArrayList;
+
 import net.minecraft.src.BlockContainer;
+import net.minecraft.src.ItemStack;
 import net.minecraft.src.Material;
 import net.minecraft.src.TileEntity;
 import net.minecraft.src.World;
@@ -29,6 +32,11 @@ public class BlockChunkLoader extends BlockContainer implements ITextureProvider
 	@Override
 	public String getTextureFile() {
 		return "/net/minecraft/src/buildcraft/additionalpipes/resources/chunkloader.png";
+	}
+	
+	@Override
+	public void addCreativeItems(ArrayList itemList) {
+		itemList.add(new ItemStack(this));
 	}
 
 }
